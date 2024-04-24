9350632bb0feec1be23c04ba2850214ebcb7f695
RaissaKestring <raissakestring23@gmail.com>
2024-04-24 14:54:05 -0300

    erro config h1 e img margin

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,9 @@
+import './Banner.css'
+
+export const Banner = () => {
+    return (
+        <header className='banner'>
+        <img className="banner" src="images/Rectangle.png" />
+        </header>
+    )
+}
---
+++ src/componentes/TitleWhite/index.js
@@ -0,0 +1,9 @@
+import './TitleWhite.css'
+
+export const TitleWhite = () => {
+    return (
+        <h1 className='title-white'>Books & Co</h1>
+    )
+}
+
+export default TitleWhite
